163c2e8b5d0edb6e26693e51e655cd983e981304
Nick Sprague <n.sprague18@gmail.com>
2024-04-04 22:29:42 -0400

    more routes changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,11 @@
 //=====================================
 // basic requires for express, sequalize, dotenv, and files.
 //=====================================
-const router1 = require('express').router();
+const router1 = require('express').Router();
 const sequelize = require('sequelize');
 const { Reviews, User, Hikes } = require('/Users/nicholassprague/bootcamp/PROJECTS/Project_2/models');
 require('dotenv').config();
 const needAuth = require('utils/auth.js');
-
-//===================================
-// for apikey
-//===================================
-const apiKey = process.env.apiKey;
 
 //===================================
 // below is a try/catch with generic error message and 500 error for bad server
@@ -23,3 +18,20 @@ router1.get('/' , async (req, res) => {
         res.status(500).send({message: `Oh No! A Generic Error Massage!`})
     }
 });
+
+router1.get('/password', async (req, res) => {
+    try {
+        res.render('password', { loggedIn: req.session.loggedIn});
+    } catch (error) {
+        console.log(error);
+        res.status(401).json(error);
+    }
+});
+
+router1.get('models/reviews.js', async (res, req) => {
+    try {
+        const response = await fetch('models/reviews.js')
+    }
+});
+const reviewData = await res.json();
+
