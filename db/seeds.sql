370ec139f3250b45f9c38924f7ca950519e489ba
ceci-00 <ceci.ga003@gmail.com>
2024-04-07 08:04:53 -0500

    added code for mysql2 and controllers routes in server.js

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_info (id, first_name, last_name, email)
+VALUES (001, "michael", "fisher", "fisherman@yahoo.com"),
+       (002, "mario", "winans", "supermario@gmail.com"),
+       (003, "sasha", "malik", "prettylady@aol.com"),
