f36cb63e6c7ec1b816df43c6809cd306732dd75d
ceci-00 <ceci.ga003@gmail.com>
2024-04-10 19:21:48 -0500

    removed seeds.sql database and created seeds.js w/access to userdata.json instead

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user_info (id, first_name, last_name, email)
-VALUES (001, "michael", "fisher", "fisherman@yahoo.com"),
-       (002, "mario", "winans", "supermario@gmail.com"),
-       (003, "sasha", "malik", "prettylady@aol.com"),
