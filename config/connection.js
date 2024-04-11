175d6d755d90bb72acd4e878bcf309c5e41afbf4
ceci-00 <ceci.ga003@gmail.com>
2024-04-10 21:07:30 -0500

    final connection in server.js and connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,23 +5,23 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
+console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 // Check if the environment variable JAWSDB_URL is set (Heroku deployment)
 if (process.env.JAWSDB_URL) {
     // Create Sequelize instance using the JAWSDB_URL environment variable
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    // If JAWSDB_URL is not set, create Sequelize instance using local database credentials
-    sequelize = new Sequelize(
-        process.env.DB_NAME,     // Database name
-        process.env.DB_USER,     // Database user
-        process.env.DB_PASSWORD, // Database password
-        {
-            host: 'localhost',   // Database host
-            dialect: 'mysql',    // Database dialect (MySQL in this case)
-            port: 3306           // Database port
-        }
-    );
+// If JAWSDB_URL is not set, create Sequelize instance using local database credentials
+sequelize = new Sequelize(
+    process.env.DB_NAME,     // Database name
+    process.env.DB_USER,     // Database user
+    process.env.DB_PASSWORD, // Database password
+    {
+        host: 'localhost',   // Database host
+        dialect: 'mysql',    // Database dialect (MySQL in this case)
+        port: 3306           // Database port
+    }
+);
 }
 
 // Export the sequelize instance to be used in other parts of the application
