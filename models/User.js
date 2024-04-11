82fabea7794b9cb8a07d3aae2888519726f5a105
ceci-00 <ceci.ga003@gmail.com>
2024-04-10 20:31:22 -0500

    created table in models/User.js and sequelized options

--- models/User.js
+++ models/User.js
@@ -9,6 +9,59 @@ class User extends Model {
     }
 }
 
-
+// Define the User model
+User.init(
+    {
+        // Define columns of the User table
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true // Ensure usernames are unique
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, // Ensure emails are unique
+            validate: {
+                isEmail: true // Validate email format
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8] // Minimum password length of 8 characters
+            }
+        }
+    },
+    {
+        // Configure sequelize options
+        hooks: {
+            // Before a user is created, hash the password
+            async beforeCreate(newUser) {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser;
+            },
+            // Before a user is updated, hash the password if it's changed
+            async beforeUpdate(updatedUser) {
+                if (updatedUser.password) {
+                    updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+                }
+                return updatedUser;
+            }
+        },
+        sequelize, // Connect model to sequelize instance
+        timestamps: false, // Disable timestamps (createdAt and updatedAt columns)
+        freezeTableName: true, // Prevent sequelize from pluralizing table names
+        underscored: true, // Use snake_case for column names
+        modelName: 'user' // Set model name to 'user'
+    }
+);
 
 module.exports = User;
